7d6add148f475b916e3f2449e2327343c60583de
Paola Moreno Reyes <nailamoreno99@gmail.com>
2022-02-21 22:36:16 -0600

    commit tree 29e569f70d538bf3a9649bf8b8dbe50b81ec6347

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const main = document.querySelector("#main");
 main.remove();
 const newHeader = document.createElement("h1");
-newHeader.setAttribute("id", "victory");
-newHeader.innerHTML = "Reid is the champion";
+newHeader.id = "victory"
+newHeader.textContent = "YOUR-NAME is the champion";
 document.body.append(newHeader);
